23f33da95d8fb4dae86ddf72a932c204b3814141
Dam <shit2wice@gmail.com>
2022-12-05 20:56:08 +0100

    feat: Add navigation to mobile view

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./Navigation.css";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <div className="nav">
+      <button className="nav__button">Next Step</button>
+      <div className="nav__back">Go Back</div>
+    </div>
+  );
 };
 
 export default Navigation;
