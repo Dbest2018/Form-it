c4e84636edc61f339f92c2ddb11f5962a94c6af5
Dam <shit2wice@gmail.com>
2022-12-05 17:51:38 +0100

    feat: Add sidebar component in mobile view

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,77 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const [selectedTab, setSelectedTab] = useState({ tab1: true });
+  const navigate = useNavigate();
+
+  const defaultStyle = {
+    color: "var(--white)",
+    backgroundColor: "transparent",
+  };
+  const activeStyle = {
+    color: "var(--marine-blue)",
+    backgroundColor: "var(--light-blue)",
+  };
+  const setActive = (tab) => {
+    if (selectedTab.tab) {
+      return;
+    }
+    setSelectedTab({
+      [tab]: true,
+    });
+    switch (tab) {
+      case "tab1":
+        navigate("/");
+        break;
+      case "tab2":
+        navigate("/plan");
+        break;
+      case "tab3":
+        navigate("/addons");
+        break;
+      case "tab4":
+        navigate("/summary");
+        break;
+      default:
+        navigate("/");
+    }
+  };
+  return (
+    <div className="sidebar">
+      <div className="sidebar__nav">
+        <div
+          className="sidebar__nav-tab"
+          style={selectedTab.tab1 ? activeStyle : defaultStyle}
+          onClick={() => setActive("tab1")}
+        >
+          1
+        </div>
+        <div
+          className="sidebar__nav-tab"
+          style={selectedTab.tab2 ? activeStyle : defaultStyle}
+          onClick={() => setActive("tab2")}
+        >
+          2
+        </div>
+        <div
+          className="sidebar__nav-tab"
+          style={selectedTab.tab3 ? activeStyle : defaultStyle}
+          onClick={() => setActive("tab3")}
+        >
+          3
+        </div>
+        <div
+          className="sidebar__nav-tab"
+          style={selectedTab.tab4 ? activeStyle : defaultStyle}
+          onClick={() => setActive("tab4")}
+        >
+          4
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
