f01ccf5d1cd5b6bd42c81dbb1fb1df39cd72a3c7
Dam <shit2wice@gmail.com>
2022-12-06 22:49:39 +0100

    feat: Add plan page

--- src/pages/plan/Plan.jsx
+++ src/pages/plan/Plan.jsx
@@ -2,7 +2,50 @@ import React from "react";
 import "./Plan.css";
 
 const Plan = () => {
-  return <div>Plan</div>;
+  return (
+    <div className="plan">
+      <div className="plan__title">Select your plan</div>
+      <div className="plan__text">
+        You have the option of monthly or yearly billing.
+      </div>
+
+      <div className="plan__options">
+        <div className="plan__option">
+          <img src="" alt="option" className="plan__option-img" />
+          <div className="plan__option-info">
+            <div className="option__info-title">Arcade</div>
+            <div className="option__info-text">$9/mo</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="plan__options">
+        <div className="plan__option">
+          <img src="" alt="option" className="plan__option-img" />
+          <div className="plan__option-info">
+            <div className="option__info-title">Advanced</div>
+            <div className="option__info-text">$12/mo</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="plan__options">
+        <div className="plan__option">
+          <img src="" alt="option" className="plan__option-img" />
+          <div className="plan__option-info">
+            <div className="option__info-title">Pro</div>
+            <div className="option__info-text">$15/mo</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="plan__packages">
+        <div className="monthly-package">Monthly</div>
+        {/* toggle icon */}
+        <div className="yearly-package">Yearly</div>
+      </div>
+    </div>
+  );
 };
 
 export default Plan;
