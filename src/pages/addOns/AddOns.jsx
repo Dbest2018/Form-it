969967e40b8eed8bd4d1e018440ace1468e5978e
Dam <shit2wice@gmail.com>
2022-12-14 00:04:12 +0100

    style: Add mobile view of addOns component

--- src/pages/addOns/AddOns.jsx
+++ src/pages/addOns/AddOns.jsx
@@ -1,8 +1,54 @@
 import React from "react";
+import Header from "../../components/header/Header";
 import "./AddOns.css";
 
 const AddOns = () => {
-  return <div>AddOns</div>;
+  return (
+    <div className="add">
+      <Header
+        title={"Pick add-ons"}
+        text={"Add-ons help enhance your gaming experience."}
+      />
+      <div className="add__options">
+        <div className="add__option">
+          <div className="option-left">
+            <input type="checkbox" />
+            <div className="add__option-info">
+              <div className="option__info-title">Online service</div>
+              <div className="option__info-text">
+                Access to multiplayer games
+              </div>
+            </div>
+          </div>
+          <div className="add__price">+$1/mo</div>
+        </div>
+
+        <div className="add__option">
+          <div className="option-left">
+            <input type="checkbox" />
+            <div className="add__option-info">
+              <div className="option__info-title">Larger storage</div>
+              <div className="option__info-text">Extra 1TB of cloud save</div>
+            </div>
+          </div>
+          <div className="add__price">+$2/mo</div>
+        </div>
+
+        <div className="add__option">
+          <div className="option-left">
+            <input type="checkbox" />
+            <div className="add__option-info">
+              <div className="option__info-title">Customizable profile</div>
+              <div className="option__info-text">
+                Custom theme on your profile
+              </div>
+            </div>
+          </div>
+          <div className="add__price">+$2/mo</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddOns;
