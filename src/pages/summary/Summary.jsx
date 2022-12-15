d8d6f23c89a5d6dab16162041474254c9d0efd38
Dam <shit2wice@gmail.com>
2022-12-15 22:35:24 +0100

    feat: Add summary component

--- src/pages/summary/Summary.jsx
+++ src/pages/summary/Summary.jsx
@@ -1,8 +1,40 @@
 import React from "react";
+import Header from "../../components/header/Header";
 import "./Summary.css";
 
 const Summary = () => {
-  return <div>Summary</div>;
+  return (
+    <div className="summary">
+      <Header
+        title={"Finishing up"}
+        text={"Double-check everything looks OK before confirming."}
+      />
+      <div className="summary__detail">
+        <div className="summary__main">
+          <div className="summary__main-type">
+            <div className="type-title">Arcade (Monthly)</div>
+            <div className="type-toggle">Change</div>
+          </div>
+          <div className="summary__main-price">$9/mo</div>
+        </div>
+        <div className="summary__extra">
+          <div className="summary__extra-package">
+            <div className="extra-title">Online service</div>
+            <div className="extra-price">+$1/mo</div>
+          </div>
+          <div className="summary__extra-package">
+            <div className="extra-title">Larger storage</div>
+            <div className="extra-price">+$2/mo</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="summary__total">
+        <div className="summary__total-title">Total (per month)</div>
+        <div className="summary__total-price">+$12/mo</div>
+      </div>
+    </div>
+  );
 };
 
 export default Summary;
