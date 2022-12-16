04cbd633bc98a74f418de3eab020a9af1ef0431e
Dam <shit2wice@gmail.com>
2022-12-16 17:24:06 +0100

    feat: Add previous and next page functionality

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,11 +1,66 @@
-import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { changeTab } from "../../features/selectedTab/selectedTabSlice";
 import "./Navigation.css";
 
 const Navigation = () => {
+  const selectedTab = useSelector((state) => state.selectedTab.value);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const nextPage = () => {
+    if (selectedTab === "tab4") {
+      return;
+    }
+    switch (selectedTab) {
+      case "tab1":
+        dispatch(changeTab("tab2"));
+        navigate("/plan");
+        break;
+      case "tab2":
+        dispatch(changeTab("tab3"));
+        navigate("/addons");
+        break;
+      case "tab3":
+        dispatch(changeTab("tab4"));
+        navigate("/summary");
+        break;
+      default:
+        dispatch(changeTab("tab1"));
+        navigate("/");
+    }
+  };
+
+  const previousPage = () => {
+    if (selectedTab === "tab1") {
+      return;
+    }
+    switch (selectedTab) {
+      case "tab2":
+        dispatch(changeTab("tab1"));
+        navigate("/");
+        break;
+      case "tab3":
+        dispatch(changeTab("tab2"));
+        navigate("/plan");
+        break;
+      case "tab4":
+        dispatch(changeTab("tab3"));
+        navigate("/addons");
+        break;
+      default:
+        dispatch(changeTab("tab1"));
+        navigate("/");
+    }
+  };
   return (
     <div className="nav">
-      <button className="nav__button">Next Step</button>
-      <div className="nav__back">Go Back</div>
+      <button className="nav__button" onClick={() => nextPage()}>
+        {selectedTab === "tab4" ? "Confirm" : "Next Step"}
+      </button>
+      <div className="nav__back" onClick={() => previousPage()}>
+        {selectedTab === "tab1" ? "" : "Go Back"}
+      </div>
     </div>
   );
 };
