77995a7edf1c1a3d665008f143952aa4a17a6cf9
Dam <shit2wice@gmail.com>
2022-12-20 23:11:55 +0100

    Feat: Display selected plan and addOns in summary

--- src/pages/summary/Summary.jsx
+++ src/pages/summary/Summary.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import { useSelector } from "react-redux";
 import Header from "../../components/header/Header";
 import "./Summary.css";
 
+const ExtraPackage = ({ addOn }) => {
+  return (
+    <div className="summary__extra">
+      <div className="summary__extra-package">
+        <div className="extra-title">{addOn.name}</div>
+        <div className="extra-price">{addOn.price}</div>
+      </div>
+    </div>
+  );
+};
+
 const Summary = () => {
+  const addOns = useSelector((state) => state.addOns.value);
+  const selectedPlan = useSelector((state) => state.selectedPlan.value);
+
   return (
     <div className="summary">
       <Header
@@ -12,21 +26,16 @@ const Summary = () => {
       <div className="summary__detail">
         <div className="summary__main">
           <div className="summary__main-type">
-            <div className="type-title">Arcade (Monthly)</div>
+            <div className="type-title">
+              {selectedPlan.name} ({selectedPlan.type})
+            </div>
             <div className="type-toggle">Change</div>
           </div>
-          <div className="summary__main-price">$9/mo</div>
+          <div className="summary__main-price">{selectedPlan.price}</div>
         </div>
-        <div className="summary__extra">
-          <div className="summary__extra-package">
-            <div className="extra-title">Online service</div>
-            <div className="extra-price">+$1/mo</div>
-          </div>
-          <div className="summary__extra-package">
-            <div className="extra-title">Larger storage</div>
-            <div className="extra-price">+$2/mo</div>
-          </div>
-        </div>
+        {addOns.map((addOn, index) => (
+          <ExtraPackage key={index} addOn={addOn} />
+        ))}
       </div>
 
       <div className="summary__total">
